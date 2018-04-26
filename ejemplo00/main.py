bb9a09d00ea2c92741bd5ff2ef1e06c74ea718bd
Sebah123 <38671458+Sebah123@users.noreply.github.com>
2018-04-26 17:46:23 -0300

    Add files via upload

---
+++ ejemplo00/main.py
@@ -0,0 +1,33 @@
+xesto es serio:
+Pruebas
+
+yo hago dos cosas a la vez xdxdxdxdxd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sdfasdf
+asdf
+asdf
+asd
+fasd
+fa
+sdf
+asdf
+asdfx
+asdfasdfasdfasdfadfadsf
+aadfasdfasdf
+asdfadsfasdf
+
+prueba para modificar xd
+
---
+++ ejemplo00/sebah/main.py
@@ -0,0 +1,9 @@
+edad()
+
+
+print("Indique su edad : ")
+
+if:(edad > 18)
+	print("Usted es mayor de edad.")
+else:
+	print("Usted es menor de edad.")
